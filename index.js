6219122493d4a739be71ea360564182e8e2424f1
Mono-Sodium-Glutamate <48638958+Mono-Sodium-Glutamate@users.noreply.github.com>
2023-03-24 14:50:12 -0700

    finished main functions

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ if (window.scrollY > 50) {
   navbar.classList.add("hidden");
 }
 });
+
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+
+function onTabClick() {
+  const tab = this.dataset.tab;
+  tabContents.forEach(content => {
+    if (content.dataset.tab === tab) {
+      content.classList.add('active');
+    } else {
+      content.classList.remove('active');
+    }
+  });
+  tabs.forEach(tab => tab.classList.remove('active'));
+  this.classList.add('active');
+}
+
+tabs.forEach(tab => tab.addEventListener('click', onTabClick));
