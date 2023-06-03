bdc29e13775d1e5d9b57172b200d463cbb18e3e3
Monosodium Glutamate <48638958+Mono-Sodium-Glutamate@users.noreply.github.com>
2023-06-02 20:11:58 -0700

    d

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ if (window.scrollY > 50) {
 }
 });
 
+// Number of stars to generate
+const numStars = 5;
+
+// Get the container element for the stars
+const nightContainer = document.querySelector('.night');
+
+// Generate random positions and delays for each star
+for (let i = 0; i < numStars; i++) {
+  const star = document.createElement('div');
+  star.classList.add('star');
+
+  // Randomize the position
+  const left = (Math.random() - Math.random()) * 200; // Random left position in percentage
+  const top = (Math.random() - Math.random()) * 200; // Random top position in percentage
+  star.style.left = `${left}%`;
+  star.style.top = `${top}%`;
+
+  // Randomize the animation delay
+  const delay = Math.random() * 7; // Random delay in seconds
+  star.style.animationDelay = `${delay}s`;
+
+  nightContainer.appendChild(star);
+}
+
 const tabs = document.querySelectorAll('.tab');
 const tabContents = document.querySelectorAll('.tab-content');
 
